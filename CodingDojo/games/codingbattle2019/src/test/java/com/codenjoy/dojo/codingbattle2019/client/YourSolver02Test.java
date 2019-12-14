058a5f4b1219fd38fe7531e49f45981d35a2b19e
wezzen <dimariqqq@gmail.com>
2019-12-14 14:35:11 +0600

    [+] YourSolver02Test: new class.

---
+++ CodingDojo/games/codingbattle2019/src/test/java/com/codenjoy/dojo/codingbattle2019/client/YourSolver02Test.java
@@ -0,0 +1,17 @@
+package com.codenjoy.dojo.codingbattle2019.client;
+
+import com.codenjoy.dojo.client.Solver;
+import com.codenjoy.dojo.codingbattle2019.client.ai.AI3Solver;
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.services.Point;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class YourSolver02Test {
+
+
+}
